a8798cd67c24b648f97a4fa885eab8c3886bbd56
Deependra Shrestha <36838592+dpenra@users.noreply.github.com>
2024-09-22 21:35:17 +0545

    Update riseos.sh

--- riseos.sh
+++ riseos.sh
@@ -7,7 +7,7 @@ rm -rf .repo/local_manifests/
 repo init -u repo init -u https://github.com/RisingTechOSS/android -b fourteen --git-lfs
 
 # Clone local_manifests repository
-git clone https://github.com/dpenra/sunny_manifest.git --depth 1 -b main .repo/local_manifests
+git clone https://github.com/dpenra/sunny_manifest --depth 1 -b main .repo/local_manifests
 
 # repo sync
 /opt/crave/resync.sh
@@ -15,7 +15,7 @@ git clone https://github.com/dpenra/sunny_manifest.git --depth 1 -b main .repo/l
 # removing and adding source
 # Set up build environment
 # initialize build environment
-source build/envsetup.sh
+. build/envsetup.sh
 
 # build
 riseup sunny user
