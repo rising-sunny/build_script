29418c87e541e9c1cb5fd71c0fb3d808dd047303
Deependra Shrestha <36838592+dpenra@users.noreply.github.com>
2024-09-24 18:09:09 +0545

    Update riseos.sh

--- riseos.sh
+++ riseos.sh
@@ -1,25 +1,73 @@
 # Clone RisingOS
 
 # Remove local manifests if they exist
-rm -rf .repo/local_manifests/
-
-# Initialize ROM manifest (with --git-lfs for large file support)
+rm -rf .repo/local_manifests
+# remove device
+rm -rf device/xiaomi/sunny
+rm -rf device/xiaomi/sunny-kernel
+rm -rf device/qcom/qssi
+rm -rf device/qcom/common
+# remove kernel
+rm -rf kernel/xiaomi/sunny
+# remove vendor
+rm -rf vendor/xiaomi/sunny
+rm -rf vendor/qcom/common
+rm -rf vendor/qcom/opensource/core-utils
+rm -rf vendor/lineage-priv/keys
+rm -rf vendor/bcr
+rm -rf vendor/xiaomi/mojito-leicacamera
+rm -rf vendor/xiaomi/miuiapps
+# remove hardware
+rm -rf hardware/xiaomi
+# remove prebuilts
+rm -rf prebuilts/gcc/linux-x86/aarch64/aarch64-elf
+rm -rf prebuilts/gcc/linux-x86/arm/arm-eabi
+# remove packages
+rm -rf packages/apps/DisplayFeatures
+rm -rf packages/apps/Kprofiles
+rm -rf packages/apps/ViPER4AndroidFX
+# remove source mod
+rm -rf hardware/qcom-caf/sm8150/media
+rm -rf packages/apps/Updater
+rm -rf vendor/lineage
+# Initialize ROM manifest
 repo init -u https://github.com/RisingTechOSS/android -b fourteen --git-lfs
-
-# Clone your custom local_manifests repository
-git clone https://github.com/dpenra/sunny_manifest --depth 1 -b main .repo/local_manifests
-
-# Sync the repository
+# repo sync
 /opt/crave/resync.sh
-
-# Set up build environment
+# remove directory
+rm -rf hardware/qcom-caf/sm8150/media
+rm -rf vendor/lineage
+rm -rf packages/apps/Updater
+# clone directory
+# device tree
+git clone https://github.com/dpenra-sunny/device_xiaomi_sunny.git --depth 1 -b fourteen device/xiaomi/sunny
+git clone https://github.com/dpenra-sunny/device_xiaomi_sunny-kernel.git --depth 1 -b fourteen device/xiaomi/sunny-kernel
+git clone https://github.com/AOSPA/android_device_qcom_qssi.git --depth 1 -b uvite device/qcom/qssi
+git clone https://github.com/yaap/device_qcom_common.git --depth 1 -b fourteen device/qcom/common
+# kernel tree
+git clone https://github.com/dpenra-sunny/kernel_xiaomi_sunny.git --depth 1 kernel/xiaomi/sunny
+# vendor tree
+git clone https://github.com/dpenra-sunny/vendor_xiaomi_sunny.git --depth 1 -b fourteen vendor/xiaomi/sunny
+git clone https://gitlab.com/yaosp/vendor_qcom_common.git --depth 1 -b fourteen vendor/qcom/common
+git clone https://github.com/yaap/vendor_qcom_opensource_core-utils.git --depth 1 -b fourteen vendor/qcom/opensource/core-utils
+git clone https://github.com/dpenra-sunny/vendor_lineage-priv_keys.git --depth 1 -b master vendor/lineage-priv/keys
+git clone https://github.com/Chaitanyakm/vendor_bcr.git --depth 1 -b main vendor/bcr
+git clone https://gitlab.com/dpenra/android_vendor_xiaomi_mojito-leicacamera.git --depth 1 -b main vendor/xiaomi/mojito-leicacamera
+git clone https://github.com/dpenra-apps/android_vendor_xiaomi_miuiapps.git --depth 1 -b main vendor/xiaomi/miuiapps
+# hardware tree
+git clone https://github.com/dpenra-sunny/hardware_xiaomi.git --depth 1 -b main hardware/xiaomi
+# prebuilts
+git clone https://github.com/StatiXOS/android_prebuilts_gcc_linux-x86_aarch64_aarch64-elf.git --depth 1 -b 14.0.0 prebuilts/gcc/linux-x86/aarch64/aarch64-elf
+git clone https://github.com/StatiXOS/android_prebuilts_gcc_linux-x86_arm_arm-eabi.git --depth 1 -b 12.0.0 prebuilts/gcc/linux-x86/arm/arm-eabi
+# packages
+git clone https://github.com/cyberknight777/android_packages_apps_DisplayFeatures.git --depth 1 -b master packages/apps/DisplayFeatures
+git clone https://github.com/KProfiles/android_packages_apps_Kprofiles.git --depth 1 -b main packages/apps/Kprofiles
+git clone https://github.com/xiaomi-begonia-dev/android_packages_apps_ViPER4AndroidFX.git --depth 1 -b fourteen packages/apps/ViPER4AndroidFX
+# source mods
+git clone https://github.com/yaap/hardware_qcom-caf_sm8150_media.git --depth 1 -b fourteen hardware/qcom-caf/sm8150/media
+git clone https://github.com/dpenra-sunny/android_packages_apps_Updater.git --depth 1 -b fourteen packages/apps/Updater
+git clone https://github.com/dpenra-sunny/android_vendor_lineage.git --depth 1 -b fourteen vendor/lineage
+# set build environment
 . build/envsetup.sh
-
-# Set up the build target (RisingOS specific command)
 riseup sunny user
-
-# Build the ROM (RisingOS-specific build command)
 rise b
-
-# Alternative Android build system command
-# mka bacon
