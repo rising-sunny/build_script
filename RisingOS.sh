dbfcd8f78e24def0f8c166e17b98ff5a4ae4a58a
Deependra Shrestha <36838592+dpenra@users.noreply.github.com>
2024-10-17 20:11:19 +0545

    Update RisingOS.sh

--- RisingOS.sh
+++ RisingOS.sh
@@ -1,15 +1,18 @@
 #!/bin/bash
 
-# Remove local manifests if they exist
+# Remove the local manifests directory if it exists (cleanup before repo initialization)
 rm -rf .repo/local_manifests/
-# remove device
+
+# Remove device trees (cleanup old device-specific configurations)
 rm -rf device/xiaomi/sunny
 rm -rf device/xiaomi/sunny-kernel
 rm -rf device/qcom/qssi
 rm -rf device/qcom/common
-# remove kernel
+
+# Remove the kernel tree for Xiaomi Sunny device
 rm -rf kernel/xiaomi/sunny
-# remove vendor
+
+# Remove vendor trees (vendor-specific files for Xiaomi, Qualcomm, and others)
 rm -rf vendor/xiaomi/sunny
 rm -rf vendor/qcom/common
 rm -rf vendor/qcom/opensource/core-utils
@@ -17,37 +20,46 @@ rm -rf vendor/lineage-priv/keys
 rm -rf vendor/bcr
 rm -rf vendor/xiaomi/mojito-leicacamera
 rm -rf vendor/xiaomi/miuiapps
-# remove hardware
+
+# Remove hardware-specific files for Xiaomi devices
 rm -rf hardware/xiaomi
-# remove prebuilts
+
+# Remove prebuilt GCC compilers (used for building the kernel)
 rm -rf prebuilts/gcc/linux-x86/aarch64/aarch64-elf
 rm -rf prebuilts/gcc/linux-x86/arm/arm-eabi
-# remove packages
+
+# Remove additional packages (DisplayFeatures, KProfiles, and ViPER4AndroidFX)
 rm -rf packages/apps/DisplayFeatures
 rm -rf packages/apps/KProfiles
 rm -rf packages/apps/ViPER4AndroidFX
-# remove source mod
+
+# Remove specific source modifications (clean up custom hardware and package sources)
 rm -rf hardware/qcom-caf/sm8150/media
 rm -rf packages/apps/Updater
 rm -rf vendor/lineage
-# Initialize ROM manifest
+
+# Initialize the ROM manifest from the RisingTechOSS repository
 repo init -u https://github.com/RisingTechOSS/android -b fourteen --git-lfs
-# repo sync
+
+# Synchronize the repo using a custom resync script
 /opt/crave/resync.sh
-# remove directory
+
+# Remove outdated or unneeded directories after syncing
 rm -rf hardware/qcom-caf/sm8150/media
 rm -rf vendor/lineage
 rm -rf packages/apps/Updater
 rm -rf frameworks/base
-# cloning DT
-# device tree
+
+# Clone the device tree repositories for Xiaomi Sunny and related kernel and common configurations
 git clone https://github.com/rising-sunny/device_xiaomi_sunny.git --depth 1 -b fourteen device/xiaomi/sunny
 git clone https://github.com/dpenra-sunny/device_xiaomi_sunny-kernel.git --depth 1 -b fourteen device/xiaomi/sunny-kernel
 git clone https://github.com/AOSPA/android_device_qcom_qssi.git --depth 1 -b uvite device/qcom/qssi
 git clone https://github.com/yaap/device_qcom_common.git --depth 1 -b fourteen device/qcom/common
-# kernel tree
+
+# (Optional) Clone the kernel tree for Xiaomi Sunny (commented out for now)
 # git clone https://github.com/dpenra-sunny/kernel_xiaomi_sunny.git --depth 1 kernel/xiaomi/sunny
-# vendor tree
+
+# Clone vendor repositories for Xiaomi Sunny, Qualcomm, and other components
 git clone https://github.com/rising-sunny/vendor_xiaomi_sunny.git --depth 1 -b fourteen vendor/xiaomi/sunny
 git clone https://gitlab.com/yaosp/vendor_qcom_common.git --depth 1 -b fourteen vendor/qcom/common
 git clone https://github.com/yaap/vendor_qcom_opensource_core-utils.git --depth 1 -b fourteen vendor/qcom/opensource/core-utils
@@ -55,22 +67,29 @@ git clone https://github.com/sunny-keys/lineage-priv-risingos.git --depth 1 -b m
 git clone https://github.com/Chaitanyakm/vendor_bcr.git --depth 1 -b main vendor/bcr
 git clone https://gitlab.com/dpenra/android_vendor_xiaomi_mojito-leicacamera.git --depth 1 -b main vendor/xiaomi/mojito-leicacamera
 git clone https://github.com/dpenra-apps/android_vendor_xiaomi_miuiapps.git --depth 1 -b main vendor/xiaomi/miuiapps
-# hardware tree
+
+# Clone the hardware-specific files for Xiaomi devices
 git clone https://github.com/dpenra-sunny/hardware_xiaomi.git --depth 1 -b fourteen hardware/xiaomi
-# prebuilts
+
+# Clone prebuilt GCC compilers for ARM and AArch64 architecture
 git clone https://github.com/StatiXOS/android_prebuilts_gcc_linux-x86_aarch64_aarch64-elf.git --depth 1 -b 14.0.0 prebuilts/gcc/linux-x86/aarch64/aarch64-elf
 git clone https://github.com/StatiXOS/android_prebuilts_gcc_linux-x86_arm_arm-eabi.git --depth 1 -b 12.0.0 prebuilts/gcc/linux-x86/arm/arm-eabi
-# packages
+
+# Clone specific packages for DisplayFeatures, KProfiles, and ViPER4AndroidFX
 git clone https://github.com/cyberknight777/android_packages_apps_DisplayFeatures.git --depth 1 -b master packages/apps/DisplayFeatures
 git clone https://github.com/KProfiles/android_packages_apps_Kprofiles.git --depth 1 -b main packages/apps/KProfiles
 git clone https://github.com/xiaomi-begonia-dev/android_packages_apps_ViPER4AndroidFX.git --depth 1 -b fourteen packages/apps/ViPER4AndroidFX
-# source mods
+
+# Clone source modifications for hardware and vendor trees specific to Qualcomm and RisingOS
 git clone https://github.com/yaap/hardware_qcom-caf_sm8150_media.git --depth 1 -b fourteen hardware/qcom-caf/sm8150/media
 git clone https://github.com/rising-sunny/android_packages_apps_Updater.git --depth 1 -b fourteen packages/apps/Updater
 git clone https://github.com/dpenra-sunny/android_vendor_lineage.git --depth 1 -b fourteen vendor/lineage
-# set build environment
+
+# Set up the build environment by sourcing the envsetup script
 . build/envsetup.sh
-# lunch
+
+# Choose the build configuration (lunch target) for Xiaomi Sunny device
 riseup sunny user
-# make
+
+# Start the build process for RisingOS (build the 'bacon' target)
 rise b
